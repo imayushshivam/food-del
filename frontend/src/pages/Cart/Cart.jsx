4395ecc25c1646409c5c1db4352ba6603a6486b5
imayushshivam <ayushshivampandey@gmail.com>
2024-05-25 17:38:47 +0530

    added and completed place_order page

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,10 +1,12 @@
 import React, { useContext } from "react";
 import "./Cart.css";
 import { StoreContext } from "../../context/StoreContext";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const { cartItems, food_list, removeFromCart, getTotalCartAmount } =
     useContext(StoreContext);
+  const navigate = useNavigate();
 
   return (
     <div className="cart">
@@ -58,7 +60,9 @@ const Cart = () => {
               <b>${getTotalCartAmount() > 0 ? getTotalCartAmount() + 16 : 0}</b>
             </div>
           </div>
-          <button>Proceed To Checkout</button>
+          <button onClick={() => navigate("/order")}>
+            Proceed To Checkout
+          </button>
         </div>
         <div className="cart-promocode">
           <div>
