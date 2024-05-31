b430a77c1046a11dcab2e294a138aa0a7b0740cc
imayushshivam <ayushshivampandey@gmail.com>
2024-05-31 17:49:34 +0530

    created order checkout and payment logic

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
 import "dotenv/config";
 import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 //app config
 //initialising app using the express pack
@@ -28,6 +29,9 @@ app.use("/api/user", userRouter);
 
 //initialise end points
 app.use("/api/cart", cartRouter);
+
+//use order router to place the users order
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
