eec009bf2ac92be7ae7634748d43d69f9433676c
Ayush <ayushshivampandey@gmail.com>
2024-06-09 23:44:34 +0530

    add myorder section to show the all orders of user.

---
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -0,0 +1,59 @@
+import React, { useContext, useEffect, useState } from "react";
+import "./MyOrders.css";
+import { StoreContext } from "../../context/StoreContext";
+import axios from "axios";
+import { assets } from "../../assets/assets";
+
+const MyOrders = () => {
+  //logics to fetch users all order data and save it in static data.
+  const { url, token } = useContext(StoreContext);
+  const [data, setData] = useState([]);
+
+  const fetchOrders = async () => {
+    //call the api and save the data in the response.
+    const response = await axios.post(
+      url + "/api/order/userorders",
+      {},
+      { headers: { token } }
+    );
+    setData(response.data.data);
+    // console.log(response.data);
+  };
+
+  useEffect(() => {
+    if (token) {
+      fetchOrders();
+    }
+  }, [token]);
+
+  return (
+    <div className="my-orders">
+      <h2>My orders</h2>
+      <div className="container">
+        {data.map((order, index) => {
+          return (
+            <div key={index} className="my-orders-order">
+              <img src={assets.parcel_icon} alt="" />
+              <p>
+                {order.items.map((item, index) => {
+                  if (index === order.items.length - 1) {
+                    return item.name + " X " + item.quantity;
+                  } else {
+                    return item.name + " X " + item.quantity + ", ";
+                  }
+                })}
+              </p>
+              <p>${order.amount}.00 </p>
+              <p>Items: {order.items.length} </p>
+              <p>
+                <span>&#x25cf</span>
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default MyOrders;
