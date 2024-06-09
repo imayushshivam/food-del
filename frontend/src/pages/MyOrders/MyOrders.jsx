25283a833a3d7556f41b377ffcf0dc52cbd1a703
Ayush <ayushshivampandey@gmail.com>
2024-06-10 01:37:23 +0530

    added and completed MyOrder section , where user can see all of its orders.

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -46,8 +46,10 @@ const MyOrders = () => {
               <p>${order.amount}.00 </p>
               <p>Items: {order.items.length} </p>
               <p>
-                <span>&#x25cf</span>
+                <span> &#x25cf; </span>
+                <b>{order.status}</b>
               </p>
+              <button>Track Order</button>
             </div>
           );
         })}
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./PlaceOrder.css";
 import { StoreContext } from "../../context/StoreContext";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const PlaceOrder = () => {
   const { getTotalCartAmount, token, food_list, cartItems, url } =
@@ -60,6 +61,16 @@ const PlaceOrder = () => {
       alert("Eroor");
     }
   };
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/cart");
+    } else if (getTotalCartAmount() === 0) {
+      navigate("/cart");
+    }
+  }, [token]);
 
   return (
     <form onSubmit={placeOrder} className="place-order">
